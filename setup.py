ef162deaf79892f2507824314658e1175b6a0b1c
Aymen Jemi <jemiaymen@gmail.com>
2023-06-13 19:58:35 +0100

    update setup.py for version and autor

--- setup.py
+++ setup.py
@@ -21,25 +21,39 @@ def get_version(rel_path):
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
+    
+def get_author(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__author__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="prestashop",
     version= get_version('prestashop/version.py'),
     description="Prestashop is a library for Python to interact with the PrestaShop's Web Service API.",
-    license="GPLv3",
-    author="Aymen Jemi (AISYSNEXT)",
+    license="GNU GPL-3",
+    author=get_author('prestashop/version.py'),
     author_email="jemiaymen@gmail.com",
     url="https://github.com/AiSyS-Next/prestashop",
     packages=find_packages(),
     install_requires=required,
     long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Topic :: Internet",
     ],
     keywords=['prestashop','e-com','e-commerce','prestashop api','api','webservice']
 )
